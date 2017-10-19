e015b664faa9d9ab80e33a1c56126a8342b5b72e
Ricardo Salguero <rsalguero3@yahoo.com>
2017-10-19 19:07:58 -0400

    Created firebase auth login screen

---
+++ app/src/main/java/com/gorrilaport/mysteryshoptools/auth/AuthUiActivity.java
@@ -0,0 +1,167 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.gorrilaport.mysteryshoptools.auth;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.MainThread;
+import android.support.annotation.StringRes;
+import android.support.annotation.StyleRes;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.AuthUI.IdpConfig;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.ui.ResultCodes;
+import com.google.android.gms.common.Scopes;
+import com.google.firebase.auth.FirebaseAuth;
+import com.gorrilaport.mysteryshoptools.R;
+import com.gorrilaport.mysteryshoptools.ui.notelist.NoteListActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import static com.firebase.ui.auth.ui.ExtraConstants.EXTRA_IDP_RESPONSE;
+
+public class AuthUiActivity extends AppCompatActivity {
+    private static final String GOOGLE_TOS_URL = "https://www.google.com/policies/terms/";
+    private static final String FIREBASE_TOS_URL = "https://www.firebase.com/terms/terms-of-service.html";
+    private static final int RC_SIGN_IN = 100;
+
+    @BindView(R.id.sign_in)
+    Button mSignIn;
+
+    @BindView(android.R.id.content)
+    View mRootView;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(this, NoteListActivity.class));
+            finish();
+        }
+
+        setContentView(R.layout.auth_ui_layout);
+        ButterKnife.bind(this);
+
+        showSignInScreen();
+    }
+
+    @OnClick(R.id.sign_in)
+    public void signIn(View view) {
+        startActivityForResult(
+                AuthUI.getInstance().createSignInIntentBuilder()
+                        .setTheme(R.style.AppTheme)
+                        .setLogo(R.drawable.alta_logo)
+                        .setProviders(getSelectedProviders())
+                        .setTosUrl(GOOGLE_TOS_URL)
+                        .setIsSmartLockEnabled(true)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    private void showSignInScreen() {
+        startActivityForResult(
+                AuthUI.getInstance().createSignInIntentBuilder()
+                        .setTheme(R.style.AppTheme)
+                         .setProviders(getSelectedProviders())
+                        .setTosUrl(GOOGLE_TOS_URL)
+                        .setIsSmartLockEnabled(false)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            handleSignInResponse(resultCode, data);
+            return;
+        }
+
+        showSnackbar(R.string.unknown_response);
+    }
+
+    @MainThread
+    private void handleSignInResponse(int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            Intent in = new Intent(this, NoteListActivity.class);
+            in.putExtra(EXTRA_IDP_RESPONSE, IdpResponse.fromResultIntent(data));
+            startActivity(in);
+            finish();
+            return;
+        }
+
+        if (resultCode == RESULT_CANCELED) {
+            showSnackbar(R.string.sign_in_cancelled);
+            return;
+        }
+
+        if (resultCode == ResultCodes.RESULT_NO_NETWORK) {
+            showSnackbar(R.string.no_internet_connection);
+            return;
+        }
+
+        showSnackbar(R.string.unknown_sign_in_response);
+    }
+
+    @MainThread
+    private List<IdpConfig> getSelectedProviders() {
+        List<IdpConfig> selectedProviders = new ArrayList<>();
+
+        selectedProviders.add(new IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build());
+
+        selectedProviders.add(
+                new IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER)
+                        .setPermissions(getGooglePermissions())
+                        .build());
+
+
+        return selectedProviders;
+    }
+
+    @MainThread
+    private void showSnackbar(@StringRes int errorMessageRes) {
+        Snackbar.make(mRootView, errorMessageRes, Snackbar.LENGTH_LONG).show();
+    }
+
+
+
+    @MainThread
+    private List<String> getGooglePermissions() {
+        List<String> result = new ArrayList<>();
+        result.add(Scopes.DRIVE_FILE);
+        return result;
+    }
+
+    public static Intent createIntent(Context context) {
+        Intent in = new Intent();
+        in.setClass(context, AuthUiActivity.class);
+        return in;
+    }
+}
