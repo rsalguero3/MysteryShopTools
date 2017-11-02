0fc22977a9158244dffae757bb52baeaa926fb4e
Ricardo Salguero <rsalguero3@yahoo.com>
2017-11-02 17:50:35 -0400

    Added log in screen with animations. Firebase adding and deleting images methods added.

--- app/src/main/java/com/gorrilaport/mysteryshoptools/ui/notelist/NoteListActivity.java
+++ app/src/main/java/com/gorrilaport/mysteryshoptools/ui/notelist/NoteListActivity.java
@@ -1,12 +1,19 @@
 package com.gorrilaport.mysteryshoptools.ui.notelist;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.StringRes;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.ActionMode;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,15 +23,24 @@ import android.view.View;
 import com.artitk.licensefragment.model.License;
 import com.artitk.licensefragment.model.LicenseType;
 import com.artitk.licensefragment.support.v4.RecyclerViewLicenseFragment;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.gorrilaport.mysteryshoptools.auth.AuthUiActivity;
+import com.gorrilaport.mysteryshoptools.core.MysteryShopTools;
 import com.gorrilaport.mysteryshoptools.ui.camera.CameraFragment;
-import com.gorrilaport.mysteryshoptools.ui.timer.TimerFragment;
 import com.mikepenz.fontawesome_typeface_library.FontAwesome;
+import com.mikepenz.google_material_typeface_library.GoogleMaterial;
 import com.mikepenz.materialdrawer.AccountHeader;
 import com.mikepenz.materialdrawer.AccountHeaderBuilder;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 import com.mikepenz.materialdrawer.model.interfaces.Nameable;
 import com.mikepenz.materialdrawer.util.KeyboardUtil;
 import com.gorrilaport.mysteryshoptools.R;
@@ -36,6 +52,8 @@ import com.gorrilaport.mysteryshoptools.util.Constants;
 
 import java.util.ArrayList;
 
+import javax.inject.Inject;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -43,13 +61,26 @@ public class NoteListActivity extends AppCompatActivity implements ActionMode.Ca
     public Toolbar toolbar;
     private AccountHeader headerResult;
     private Drawer drawer;
+    private String mUsername;
+    private String mPhotoUrl;
+    private String mEmailAddress;
+    private FirebaseUser mFirebaseUser;
+
     @BindView(R.id.fab) FloatingActionButton mFab;
+    @BindView(android.R.id.content) View mRootView;
+
+    @Inject
+    NoteListContract.FirebaseRepository mFirebaseRepository;
+    @Inject NoteListContract.Repository mRepository;
 
     /**
      * Whether or not the activity is in two-pane mode, i.e. running on a tablet
      * device.
      */
     private boolean mTwoPane;
+
+    public NoteListActivity() {
+        MysteryShopTools.getInstance().getAppComponent().inject(this);}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,6 +89,22 @@ public class NoteListActivity extends AppCompatActivity implements ActionMode.Ca
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         setupNavigationDrawer(savedInstanceState);
+
+
+
+        if(mFirebaseRepository.getFirebaseUser() == null){
+            //Not signed in, launch the sign in activity
+            startActivity(new Intent(this, AuthUiActivity.class));
+            finish();
+            return;
+        }
+        else {
+            mFirebaseUser = mFirebaseRepository.getFirebaseUser();
+            mUsername = mFirebaseUser.getDisplayName();
+            //mPhotoUrl = mFirebaseUser.getPhotoUrl().toString();
+            mEmailAddress = mFirebaseUser.getEmail();
+        }
+
         setupDefaultFragment();
         ButterKnife.bind(this);
     }
@@ -83,10 +130,21 @@ public class NoteListActivity extends AppCompatActivity implements ActionMode.Ca
     }
 
     private void setupNavigationDrawer(Bundle savedInstanceState) {
+        mUsername = TextUtils.isEmpty(mUsername) ? "Anonymous" : mUsername;
+        mEmailAddress = TextUtils.isEmpty(mEmailAddress) ? "Email" : mEmailAddress;
+        mPhotoUrl = TextUtils.isEmpty(mPhotoUrl) ? "R.drawable.anon_user_48dp" : mPhotoUrl;
+
+        IProfile profile = new ProfileDrawerItem()
+                .withName(mUsername)
+                .withEmail("someemail@gymmail.com")
+                .withIcon(mPhotoUrl)
+                .withIdentifier(102);
+
         //Create Navigation drawer Account Header
         headerResult = new AccountHeaderBuilder()
                 .withActivity(this)
                 .withHeaderBackground(R.drawable.header_3)
+                .addProfiles(profile)
                 .build();
 
         drawer = new DrawerBuilder()
@@ -101,7 +159,8 @@ public class NoteListActivity extends AppCompatActivity implements ActionMode.Ca
                         new PrimaryDrawerItem().withName(R.string.menu_drawer_settings).withIcon(FontAwesome.Icon.faw_cog).withIdentifier(Constants.SETTINGS),
                         //new PrimaryDrawerItem().withName("Timer").withIcon(FontAwesome.Icon.faw_book).withIdentifier(Constants.TIMER)
                         new PrimaryDrawerItem().withName(R.string.menu_drawer_companies).withIcon(FontAwesome.Icon.faw_industry).withIdentifier(Constants.COMPANIES),
-                        new PrimaryDrawerItem().withName(R.string.menu_drawer_about).withIcon(FontAwesome.Icon.faw_database).withIdentifier(Constants.ABOUT)
+                        new PrimaryDrawerItem().withName(R.string.menu_drawer_about).withIcon(FontAwesome.Icon.faw_database).withIdentifier(Constants.ABOUT),
+                        new PrimaryDrawerItem().withName("Logout").withIcon(GoogleMaterial.Icon.gmd_lock).withIdentifier(Constants.LOGOUT)
                 )
                 .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
                     @Override
@@ -139,6 +198,7 @@ public class NoteListActivity extends AppCompatActivity implements ActionMode.Ca
                 .withFireOnInitialOnClick(true)
                 .withSavedInstance(savedInstanceState)
                 .build();
+        drawer.addStickyFooterItem(new PrimaryDrawerItem().withName(R.string.delete_account).withIcon(GoogleMaterial.Icon.gmd_delete).withIdentifier(Constants.DELETE_ACCOUNT));
     }
 
     /**
@@ -156,9 +216,6 @@ public class NoteListActivity extends AppCompatActivity implements ActionMode.Ca
             case Constants.CATEGORY:
                 startActivity(new Intent(NoteListActivity.this, CategoryActivity.class));
                 break;
-            case Constants.TIMER:
-                startActivity(new Intent(NoteListActivity.this, TimerFragment.class));
-                break;
             case Constants.CAMERA:
                 openFragment(CameraFragment.newInstance(), "Camera");
                 break;
@@ -172,7 +229,67 @@ public class NoteListActivity extends AppCompatActivity implements ActionMode.Ca
                 f.addCustomLicense(createLicenses());
                 mFab.hide();
                 openFragment(f, "About");
+                break;
+            case Constants.LOGOUT:
+                logout();
+                break;
+            case Constants.DELETE_ACCOUNT:
+                deleteAccountClicked();
         }
+
+    }
+
+    private void deleteAccountClicked() {
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setMessage("Are you sure you want to delete this account?")
+                .setPositiveButton("Yes, nuke it!", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        deleteAccount();
+                    }
+                })
+                .setNegativeButton("No", null)
+                .create();
+
+        dialog.show();
+    }
+
+    private void logout() {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            mRepository.deleteDatabase();
+                            startActivity(new Intent(NoteListActivity.this, AuthUiActivity.class));
+                            if (Build.VERSION.SDK_INT >= 21){
+                                finishAfterTransition();
+                            }
+                            else {
+                                finish();
+                            }
+                        } else {
+                            showSnackbar(R.string.sign_out_failed);
+                        }
+                    }
+                });
+
+    }
+
+    private void deleteAccount() {
+        FirebaseAuth.getInstance().getCurrentUser().delete()
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            startActivity(new Intent(NoteListActivity.this, AuthUiActivity.class));
+                            finish();
+                        } else {
+                            showSnackbar(R.string.delete_account_failed);
+                        }
+                    }
+                });
     }
 
     public void showTwoPane(Note note){
@@ -246,4 +363,9 @@ public class NoteListActivity extends AppCompatActivity implements ActionMode.Ca
         }
         openFragment(NoteListFragment.newInstance(mTwoPane), getString(R.string.note_list_activity_actionbar_name));
     }
+
+    private void showSnackbar(@StringRes int errorMessageRes) {
+        Snackbar.make(mRootView, errorMessageRes, Snackbar.LENGTH_LONG).show();
+    }
+
 }
